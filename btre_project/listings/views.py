52767e3153994e2b376e7f32087cb91859e330ce
viseth89 <viseth89@gmail.com>
2018-12-30 12:19:41 -0600

    finished video 40, pulling data from models

--- btre_project/listings/views.py
+++ btre_project/listings/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Listing
+
 def index(request):
-  return render(request, 'listings/listings.html')
+  listings = Listing.objects.all()
+
+  context = {
+    'listings': listings
+  }
+  return render(request, 'listings/listings.html', context)
 
 def listing(request):
   return render(request, 'listings/listing.html')
 
 def search(request):
   return render(request, 'listings/search.html')
+
