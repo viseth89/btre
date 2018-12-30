2b10b22c89e90e111a14ac61b4aae5adaf9dbc18
viseth89 <viseth89@gmail.com>
2018-12-30 11:51:21 -0600

    finished video 39, admin panel work

--- btre_project/listings/admin.py
+++ btre_project/listings/admin.py
@@ -3,4 +3,11 @@ from .models import Listing
 
 # Register your models here.
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'is_published', 'list_date', 'realtor' )
+  list_display_links = ('id', 'title')
+  list_filter = ('realtor',)
+  list_editable = ('is_published',)
+  search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode')
+  list_per_page = 25
+admin.site.register(Listing, ListingAdmin)
