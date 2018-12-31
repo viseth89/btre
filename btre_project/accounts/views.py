5a8be402d79a730cb7192c6cadd42ea2769e20d4
viseth89 <viseth89@gmail.com>
2018-12-31 16:30:14 -0600

    finish #50 message alerts

--- btre_project/accounts/views.py
+++ btre_project/accounts/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
 # Create your views here.
 def register(request):
   if request.method == 'POST':
-    print('SUBMITTED REG')
+    #Register User 
+    messages.error(request, 'testing error message')
     return redirect('register')
   else:
     return render(request, 'accounts/register.html')
 
 def login(request):
-  return render(request, 'accounts/login.html')
+  if request.method == 'POST':
+    #Login User 
+    return redirect('register')
+  else:
+    return render(request, 'accounts/login.html')
 
 def logout(request):
   return redirect('index')
