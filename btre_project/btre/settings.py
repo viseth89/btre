c0dc6ac9c1eaae2e34a96d431891d411ee7cc7f5
viseth89 <viseth89@gmail.com>
2019-01-01 19:32:01 -0600

    problem commit

--- btre_project/btre/settings.py
+++ btre_project/btre/settings.py
@@ -142,3 +142,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='viseth89@gmail.com'
+EMAIL_HOST_PASSWORD='Powerade2018'
+EMAIL_USE_TLS=True
